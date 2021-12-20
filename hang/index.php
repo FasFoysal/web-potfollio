62a2898f545bb9f8826cf4d66ef5028ff2db10f8
FasFoysal <shounfoysal@gmail.com>
2021-12-20 19:12:32 +1100

    hang git commit

---
+++ hang/index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="style.css">
+
+<?php 
+
+ $cricket = "1";
+  while($cricket < 100 ){
+
+    echo " <h2  > WELLCOME TO  @FAS FOYSAL AHAMED SHOUN WEBSITE || !! </h2> ";
+ }
+
+
+
+
+
+?>
